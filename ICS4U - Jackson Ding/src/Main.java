2cb5eebc148a124ef4e7667ed2c781b2cdc476e0
JacksonDing123 <122178040+JacksonDingJD@users.noreply.github.com>
2023-10-30 21:54:30 -0400

    homework 3.6

--- ICS4U - Jackson Ding/src/Main.java
+++ ICS4U - Jackson Ding/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int length, x= 3, y = 27;
+        switch(x){
+            case 1:
+                System.out.println(y*0.1);
+                break;
+            case 2:
+                System.out.println(y*0.2);
+                break;
+            case 3:
+                System.out.println(y*0.2);
+                break;
+            case 4:
+                System.out.println(y*0.15);
+                break;
+            case 5:
+                System.out.println(y*0.15);
+                break;
+            case 6:
+                System.out.println(y*0.2);
+                break;
+            }
+        }
     }
-}
