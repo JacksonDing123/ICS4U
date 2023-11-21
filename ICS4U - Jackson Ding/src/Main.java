a49a3393a1178570ce890cb9cc82ab64d9ba6002
JacksonDing123 <122178040+JacksonDingJD@users.noreply.github.com>
2023-11-21 11:30:59 -0500

    update JDK

--- ICS4U - Jackson Ding/src/Main.java
+++ ICS4U - Jackson Ding/src/Main.java
@@ -1,25 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int length, x= 3, y = 27;
-        switch(x){
-            case 1:
-                System.out.println(y*0.1);
-                break;
-            case 2:
-                System.out.println(y*0.2);
-                break;
-            case 3:
-                System.out.println(y*0.2);
-                break;
-            case 4:
-                System.out.println(y*0.15);
-                break;
-            case 5:
-                System.out.println(y*0.15);
-                break;
-            case 6:
-                System.out.println(y*0.2);
-                break;
-            }
+        double i, j, sum;
+        for(sum = 0, i = 1, j = 2; i<20&&j<21; i++, j++){
+            sum+=(double)i/j;
         }
+
+        System.out.println(sum);
     }
+}
