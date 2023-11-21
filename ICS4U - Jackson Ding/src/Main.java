b29ec634056e6fb01d2213a49f413051d41dc741
JacksonDing123 <122178040+JacksonDingJD@users.noreply.github.com>
2023-11-21 11:37:55 -0500

    deleted unneccesary idea

--- ICS4U - Jackson Ding/src/Main.java
+++ ICS4U - Jackson Ding/src/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
         double i, j, sum;
